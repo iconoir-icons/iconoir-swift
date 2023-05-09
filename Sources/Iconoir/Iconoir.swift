1f263488ea73a3da64fbaee96772e07b70a9c6ef
mkaulfers <matthew.kaulfers@yahoo.com>
2023-05-08 23:17:56 -0500

    main: Updates to 6.8.0 and fixes missing icons.

--- Sources/Iconoir/Iconoir.swift
+++ Sources/Iconoir/Iconoir.swift
@@ -1,6 +1,6 @@
 // Created by: Matthew Kaulfers
 // License: MIT
-// Version: 6.6.0
+// Version: 6.8.0
 #if canImport(UIKit)
 import UIKit
 
@@ -8,6 +8,13 @@ public extension Iconoir {
     /// Returns a UIImage of the Iconoir icon.
     var asUIImage: UIImage? {
         return UIImage(named: self.rawValue, in: Bundle.module, compatibleWith: nil)
+    }
+    
+    /// Returns an image, from the Iconoir bundle, if there is a matching key, otheriwse nil.
+    /// - Parameter string: key that matches iconoir icon.
+    /// - Returns: UIKit UIImage
+    static func uiImage(from string: String) -> UIImage? {
+        return UIImage(named: string, in: Bundle.module, compatibleWith: nil)
     }
 }
 #endif
@@ -20,6 +27,15 @@ public extension Iconoir {
     /// Returns a SwiftUI Image of the Iconoir icon.
     var asImage: Image {
         return Image(self.rawValue, bundle: Bundle.module)
+            .renderingMode(.template)
+    }
+    
+    
+    /// Returns an image, from the Iconoir bundle, if there is a matching key.
+    /// - Parameter string: key that matches iconoir icon.
+    /// - Returns: SwiftUI Image
+    static func image(from string: String) -> Image {
+        return Image(string, bundle: Bundle.module)
             .renderingMode(.template)
     }
 }
